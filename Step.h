19a3e1cf86a9308f4ae75bafccc3b0088745d69d
Kostyantyn Smozhevskykh <lee.ross2005@gmail.com>
2024-04-09 14:32:53 +0300

    add a new class Step with has-a relation, use in it operator+ overloading

---
+++ Step.h
@@ -0,0 +1,67 @@
+//
+// Created by donni on 08.04.2024.
+//
+
+#ifndef OOP4_STEP_H
+#define OOP4_STEP_H
+#include "iostream"
+#include "marks.h"
+using namespace std;
+
+class Step {
+private:
+    int step;
+public:
+    Step() : step(1000) {
+    }
+
+    Step operator+(const Step& step1) const {
+        Step result;
+        result.step = step + step1.step;
+        return result;
+    }
+
+    void updateScholarship(const Marks& marks) {
+        for (int i = 0; i < marks.size; ++i) {
+            switch (marks.MarksArray[i]) {
+                case 5:
+                    step += 150;
+                    break;
+                case 4:
+                    step += 100;
+                    break;
+                case 3:
+                    step += 50;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    void print() {
+        cout << "Your scholarship is = " << step << endl;
+    }
+
+    class TypeStep {
+    public:
+        void determineScholarship(const Marks& marks) {
+            int count = 0;
+
+            for (int i = 0; i < marks.size; ++i) {
+                if (marks.MarksArray[i] == 5) {
+                    count++;
+                }
+            }
+
+            if (count >= 3) {
+                cout << "This student is an excellent student." << endl;
+            } else {
+                cout << "This student is an ordinary student." << endl;
+            }
+        }
+    };
+};
+
+
+#endif //OOP4_STEP_H
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "marks.h"
 #include "VLK.h"
 #include "proh.h"
+#include "Step.h"
 using namespace std;
 
 int main() {
@@ -16,6 +17,19 @@ int main() {
     Marks mark(MarksArray, size);
     mark.print();
 
+    Step step1;
+    step1.updateScholarship(mark);
+    step1.print();
+    Step::TypeStep typeStep1;
+    typeStep1.determineScholarship(mark);
+
+    proh markProh1(MarksArray, size);
+    if (markProh1.pass()) {
+        cout << "The student passed." << endl;
+    } else {
+        cout << "The student did not pass." << endl;
+    }
+
     Student id2("Andriy Kravchuk", 17, 255, 660888760);
     id2.print();
 
@@ -23,6 +37,19 @@ int main() {
     int size2 = 4;
     Marks mark2(MarksArray2, size2);
     mark2.print();
+
+    Step step2;
+    step2.updateScholarship(mark2);
+    step2.print();
+    Step::TypeStep typeStep2;
+    typeStep2.determineScholarship(mark2);
+
+    proh markProh2(MarksArray2, size2);
+    if (markProh2.pass()) {
+        cout << "The student passed." << endl;
+    } else {
+        cout << "The student did not pass." << endl;
+    }
 
     int x;
     cout << "Write your VLK status(0/1) ";
