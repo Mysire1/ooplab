9f76a41328a51d828d78b2db0806bc7c86aa0c92
Kostyantyn Smozhevskykh <lee.ross2005@gmail.com>
2024-05-06 23:12:20 +0300

    FINALLY FIX FILE SYSTEM PEREMOGAAAAAAA

--- AddStudent.h
+++ AddStudent.h
@@ -2,10 +2,10 @@
 // Created by donni on 24.04.2024.
 //
 
-#ifndef OOP_LAB2_ADDSTUDENT_H
-#define OOP_LAB2_ADDSTUDENT_H
-#include "iostream"
+#include <iostream>
+#include <fstream>
 #include "student.h"
+
 using namespace std;
 
 class AddStudent : public Student {
@@ -13,10 +13,22 @@ class AddStudent : public Student {
 public:
     AddStudent() : Student("", 0, 0, 0, "", 0) {}
 
-    void addStudent() override {
+    string toString() const {
+        string result;
+        result += "Name: " + name + "\n";
+        result += "Age: " + to_string(age) + "\n";
+        result += "Room: " + to_string(RoomNumber) + "\n";
+        result += "Telephone number: " + to_string(number) + "\n";
+        result += "Faculty Name: " + facultyName + "\n";
+        result += "Faculty Grade: " + to_string(facultyGrade) + "\n";
+        return result;
+    }
+
+    void addStudent() {
         cout << "Write new data about student! " << endl;
         cout << "Name: ";
-        cin >> name;
+        cin.ignore();
+        getline(cin, name);
         cout << "Age: ";
         cin >> age;
         cout << "Room: ";
@@ -24,28 +36,9 @@ public:
         cout << "Telephone number (9 num): ";
         cin >> number;
         cout << "Name of your faculty: ";
-        cin >> facultyName;
+        cin.ignore();
+        getline(cin, facultyName);
         cout << "Your faculty grade: ";
         cin >> facultyGrade;
-
-        ofstream outFile("info.txt",ios::app);
-        if (outFile.is_open()) {
-            cout << "====================================";
-            outFile << "Name: " << name << endl;
-            outFile << "Age: " << age << endl;
-            outFile << "Room: " << RoomNumber << endl;
-            outFile << "Telephone number: " << number << endl;
-            outFile << "Faculty Name: " << facultyName << endl;
-            outFile << "Faculty Grade: " << facultyGrade << endl;
-            cout << "====================================";
-            outFile.close();
-        }
-        else {
-            cout << "File dont open ";
-        }
     }
-
 };
-
-
-#endif //OOP_LAB2_ADDSTUDENT_H
--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main() {
     int choice, choice1;
     cout << "Who are you? \n 1.Teacher \n 2.Student" << endl;
     cin >> choice1;
-    string path = R"(C:\Users\donni\CLionProjects\oop_lab2\info)";
+    string path = R"(C:\Users\donni\CLionProjects\oop_lab2\infor.txt)";
     if (choice1 == 1) {
         int attemps = 3;
         ofstream out;
@@ -24,86 +24,88 @@ int main() {
             cin >> inputpass;
             if (inputpass == teacherpass) {
                 out.open(path, ofstream::app);
-                do {
-                    cout << "Choice option \n 1.Information \n 2.Add student \n 3.Add mark \n 4.Exit \n";
-                    cin >> choice;
-                    switch (choice) {
-                        case 1: {
+                if (out.is_open()) {
+                    do {
+                        cout << "Choice option \n 1.Information \n 2.Add student \n 3.Add mark \n 4.Exit \n";
+                        cin >> choice;
+                        switch (choice) {
+                            case 1: {
+                                Student id("Kostya Smozhevskykh", 18, 399, 660878630, "Software Engineer", 1);
+                                id.print();
 
-                            Student id("Kostya Smozhevskykh", 18, 399, 660878630, "Software Engineer", 1);
-                            id.print();
-                            Step step1;
+                                int MarksArray[] = {5, 3, 3};
+                                int size = 3;
+                                Marks mark(MarksArray, size);
+                                mark.print();
 
-                            int MarksArray[] = {5, 3, 3};
-                            int size = 3;
-                            Marks mark(MarksArray, size);
-                            mark.print();
+                                Step step1;
+                                step1.updateScholarship(mark);
+                                step1.print();
 
-                            step1.updateScholarship(mark);
-                            step1.print();
-                            Step::TypeStep typeStep1;
-                            typeStep1.determineScholarship(mark);
+                                Step::TypeStep typeStep1;
+                                typeStep1.determineScholarship(mark);
 
-                            proh markProh1(MarksArray, size);
-                            markProh1.pass();
+                                proh markProh1(MarksArray, size);
+                                markProh1.pass();
 
-                            Student id2("Andriy Kravchuk", 17, 255, 660888760, "Computer Science", 1);
-                            id2.print();
+                                out << "File has been written" << endl;
 
-                            int MarksArray2[] = {5, 2, 5, 5};
-                            int size2 = 4;
-                            Marks mark2(MarksArray2, size2);
-                            mark2.print();
-
-                            Step step2;
-                            step2.updateScholarship(mark2);
-                            step2.print();
-                            Step::TypeStep typeStep2;
-                            typeStep2.determineScholarship(mark2);
-
-                            proh markProh2(MarksArray2, size2);
-                            markProh2.pass();
-
-                            cout << "File has been written" << endl;
-                            break;
-                        }
-                        case 2: {
-                            AddStudent newStudent;
-                            newStudent.addStudent();
-                            newStudent.print();
-
-                            break;
-                        }
-                        case 3: {
-                            int MarksArray[] = {5, 3, 3};
-                            int size = 3;
-                            Marks mark(MarksArray, size);
-
-                            int markw;
-                            cout << "Write mark you want to add(0-5): ";
-                            cin >> markw;
-                            while (markw > 5){
-                                cout << "Choose between 0-5: ";
-                                cin >> markw;
+                                break;
                             }
-                            mark.addMark(markw);
-                            mark.print();
+                            case 2: {
+                                AddStudent newStudent;
+                                newStudent.addStudent();
+                                out << newStudent.toString();
+                                newStudent.print();
 
-                            break;
+                                break;
+                            }
+                            case 3: {
+                                int MarksArray[] = {5, 3, 3};
+                                int size = 3;
+                                Marks mark(MarksArray, size);
+
+                                int markw;
+                                cout << "Write mark you want to add(0-5): ";
+                                cin >> markw;
+                                while (markw > 5){
+                                    cout << "Choose between 0-5: ";
+                                    cin >> markw;
+                                }
+                                mark.addMark(markw);
+                                mark.print();
+
+                                Step step1;
+                                step1.updateScholarship(mark);
+                                step1.print();
+
+                                Step::TypeStep typeStep1;
+                                typeStep1.determineScholarship(mark);
+
+                                proh markProh1(MarksArray, size);
+                                markProh1.pass();
+
+                                out << "Mark has been added" << endl; // Write to file
+
+                                break;
+                            }
+                            case 4: {
+                                attemps = 0;
+                                cout << "Bye!";
+                                break;
+                            }
+                            default:
+                                cout << "Invalid choice. Please choose again." << endl;
                         }
-                        case 4: {
-                            attemps = 0;
-                            cout << "Bye!";
-                            break;
-                        }
-                        default:
-                            cout << "Invalid choice. Please choose again." << endl;
-                    }
-                } while (choice != 4);
-                out.close();
+                    } while (choice != 4);
+                    out.close();
+                } else {
+                    cout << "Unable to open the file." << endl;
+                    return 1;
+                }
             } else {
                 attemps--;
-                cout << "You have only " << attemps << " .Try again!" << endl;
+                cout << "You have only " << attemps << " attempts. Try again!" << endl;
             }
         } while (attemps > 0);
     }
@@ -131,27 +133,10 @@ int main() {
                     proh markProh1(MarksArray, size);
                     markProh1.pass();
 
-                    Student id2("Andriy Kravchuk", 17, 255, 660888760, "Computer Science", 1);
-                    id2.print();
-
-                    int MarksArray2[] = {5, 2, 5, 5};
-                    int size2 = 4;
-                    Marks mark2(MarksArray2, size2);
-                    mark2.print();
-
-                    Step step2;
-                    step2.updateScholarship(mark2);
-                    step2.print();
-                    Step::TypeStep typeStep2;
-                    typeStep2.determineScholarship(mark2);
-
-                    proh markProh2(MarksArray2, size2);
-                    markProh2.pass();
-
                     break;
                 }
                 default:
-                    cout << "Infalid choice. Please choose again." << endl;
+                    cout << "Invalid choice. Please choose again." << endl;
             }
         } while (choice != 2);
 
